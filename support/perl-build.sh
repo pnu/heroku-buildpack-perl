8550551d1d428a56f861ecb9cdd1e38247c36c18
Panu Ervamaa <panu.ervamaa@frantic.com>
2014-03-12 13:15:39 +0200

    add support/perl-build for creating vendored perl

---
+++ support/perl-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git clone git://github.com/tokuhirom/Perl-Build.git perl-build
+perl-build/perl-build $PERL_VERSION /app/vendor/perl
+tar czf perl-$PERL_VERSION.tgz -C /app/vendor/perl .
+
+git clone https://github.com/s3tools/s3cmd
+cd s3cmd
+git checkout v1.5.0-beta1
+
+cat >~/.s3cfg <<EOF
+[default]
+access_key = $AWS_ACCESS_KEY_ID
+secret_key = $AWS_SECRET_ACCESS_KEY
+use_https = True
+EOF
+
+./s3cmd put --acl-public ~/perl-$PERL_VERSION.tgz s3://$S3_BUCKET_NAME
